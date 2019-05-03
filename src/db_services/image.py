839e9a58bf550a13ac848f2682d52da8703fee3e
Qi Feng <43156516+lullaby1024@users.noreply.github.com>
2019-05-03 03:03:14 -0400

    Delete image.py

--- src/db_services/image.py
+++
@@ -1,9 +0,0 @@
-import tornado.web
-from util import Event
-import sys
-
-class ImageHandler(tornado.web.RequestHandler):
-    def get_image():
-        path = input('Enter the image path')
-        #path = sys.stdin.readline()
-        return path
