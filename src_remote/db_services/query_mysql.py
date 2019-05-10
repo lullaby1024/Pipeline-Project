8c9b7fd59ecab53d8a42f8d52c25aee76a10251f
Qi Feng <43156516+lullaby1024@users.noreply.github.com>
2019-05-10 18:25:11 -0400

    cloud version updated

---
+++ src_remote/db_services/query_mysql.py
@@ -0,0 +1,78 @@
+# import pymysql
+from sqlalchemy import create_engine
+# from nltk.corpus import wordnet as wn
+# from pyathena import connect
+
+
+def query(ingredients):
+    # cnx = pymysql.connect(host='localhost',
+    #                       user='root',
+    #                       password='dbuserdbuser',
+    #                       db='pipeline_project',
+    #                       charset='utf8mb4',
+    #                       cursorclass=pymysql.cursors.DictCursor)
+    #
+    # cursor = cnx.cursor()
+
+    # Connect to s3 database
+    # ACCESS_KEY_ID = 'AKIAI63EPGLJ4NNTSCNQ'
+    # ACCESS_KEY = 'uMFYlQPyj5YgW19xKakqzGz0fCEsqmDWUi9fFAPP'
+    # bucket = 'recipecialist'
+    #
+    # cursor = connect(aws_access_key_id = ACCESS_KEY_ID,
+    #                  aws_secret_access_key=ACCESS_KEY,
+    #                  s3_staging_dir='s3://'+ bucket,
+    #                  region_name='us-east-1',
+    #                  cursorclass=pymysql.cursors.DictCursor).cursor()
+
+    # Set up the DB credentials.
+    DB_USER = "zc2425"
+    DB_PASSWORD = "TXePyLYulh"
+    DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
+
+    DATABASEURI = "postgresql://" + DB_USER + ":" + DB_PASSWORD + "@" + DB_SERVER + "/w4111"
+
+    engine = create_engine(DATABASEURI)
+    cnx = engine.raw_connection()
+    cursor = cnx.cursor()
+
+    #TODO: modify query and input
+
+    # food = wn.synset('food.n.02')
+    # food_dict = list(set([w for s in food.closure(lambda s: s.hyponyms()) for w in s.lemma_names()]))
+
+    # Parse sql query.
+    q = "SELECT * FROM recipes WHERE "
+    for i in ingredients:
+        if i == ingredients[-1]: # If i is the last element
+            q += "ingredients LIKE '%" + i + "%'"
+        else:
+            q += "ingredients LIKE '%" + i + "%' OR "
+
+    result = cursor.execute(q)
+    result = cursor.fetchall()
+
+    cnx.commit()
+    cnx.close()
+
+    title = []
+    ingredient = []
+    insturctions = []
+
+    for line in result:
+
+        title.append(line[0])
+        ingredient.append(line[1])
+        insturctions.append(line[2])
+
+    all_result = []
+    for i in range(0, len(title)):
+        one_result_row = []
+        one_result_row.append(title[i])
+        one_result_row.append(ingredient[i])
+        one_result_row.append(insturctions[i])
+        all_result.append(one_result_row)
+        one_result_row = []
+
+    # return result
+    return all_result
---
+++ src_remote/handlers/upload.py
@@ -0,0 +1,17 @@
+import tornado.web
+
+
+class UploadHandler(tornado.web.RequestHandler):
+
+    def post(self):
+
+        file = self.request.files['image_file'][0]
+
+        output_file = open("./uploads/" + 'test_img.jpg', 'wb')
+        # output_file = open("../uploads/" + file['filename'], 'wb')
+        output_file.write(file['body'])
+
+        self.finish("file " + file['filename'] + " is uploaded")
+
+
+
